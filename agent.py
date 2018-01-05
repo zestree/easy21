5e568c2fa699c2d607de1e5760b88251c28e2d13
edwinliu <edwin.nz@gmail.com>
2018-01-05 16:42:29 +1300

    Implement epsilon-greedy policy

--- agent.py
+++ agent.py
@@ -4,16 +4,21 @@ from env.actions import Action
 
 
 def mc_control_epsilon_greedy(env, num_episodes, discount_factor=1.0, epsilon=0.1):
-#    W = defaultdict(float)
+
     N_s = defaultdict(float)
     W = get_defaultdict_stateaction(env.action_spaces)
     N_sa = get_defaultdict_stateaction(env.action_spaces)
     Q = get_defaultdict_stateaction(env.action_spaces)
-    
-    policy = make_epsilon_greedy_policy(Q, epsilon, env.action_spaces)
+    N0 = 100
+    policy = None
 
     for episode in range(1, num_episodes):
         state = env.getInitState()
+        epsilon = N0 / (N0 + N_s[tuple(state)])
+        print epsilon
+
+        policy = make_epsilon_greedy_policy(Q, epsilon, env.action_spaces)
+
         run_episode(policy, state, env, Q, N_s, N_sa, W, discount_factor)
 
     return Q, policy
@@ -36,14 +41,17 @@ def run_episode(policy, state, env, Q, N_s, N_sa, W, discount_factor):
     return new_state, reward * discount_factor
 
 
-def get_defaultdict_stateaction(action_spaces):
-    return defaultdict(lambda: np.zeros(action_spaces))
-
-
 def make_epsilon_greedy_policy(Q, epsilon, nA):
 
     def policy_fn(state):
-
-        return np.array([np.random.rand(), np.random.rand()])
+        if np.random.rand() <= (1 - epsilon):
+            return Q[tuple(state)]
+        else:
+            rand = np.random.rand()
+            return np.array([rand, 1 - rand])
 
     return policy_fn
+
+
+def get_defaultdict_stateaction(action_spaces):
+    return defaultdict(lambda: np.zeros(action_spaces))
